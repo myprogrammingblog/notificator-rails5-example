12420b6c7dc33b24394d460b9abd9a0398fd048f
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-13 14:30:42 -0700

    db schema with messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813212603) do
+ActiveRecord::Schema.define(version: 20160813212826) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notifications", force: :cascade do |t|
     t.string   "event"
