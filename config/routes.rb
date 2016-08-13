05b57aa8a2bd90b50bb98d04d16b7e77cef40b0d
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-13 14:50:48 -0700

    Create subscribe channek notification_channel and added actioncable mount route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'notifications#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Websockets resemble this URL
+  mount ActionCable.server => '/cable'
 end
