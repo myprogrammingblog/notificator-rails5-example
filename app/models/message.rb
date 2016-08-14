36fde7555223c1064f88aacd86bb83ab01ff34b7
Anatoly Spektor <myprogrammingblog@gmail.com>
2016-08-14 16:31:53 -0700

    Make sure demo resets after 20 messa are created

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+
+  after_create_commit { reload_demo }
   after_create_commit { notify }
 
   private
@@ -6,4 +8,11 @@ class Message < ApplicationRecord
   def notify
     Notification.create(event: "New Notification (#{self.body})")
   end
+
+  # For demo purposes, clearing up notifications if there are too much of them
+  def reload_demo(max=20)
+    Message.destroy_all if Message.count >= max
+    Notification.destroy_all if Notification.count >= max
+  end
+
 end
